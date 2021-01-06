c592e641ee0355514bd074b5a9eeddca7e8ea591
Jordan Kelly <jordan.j.kelly101@gmail.com>
2021-01-06 07:58:45 -0800

    record longitude and latitude

--- script.js
+++ script.js
@@ -59,24 +59,33 @@ $("#submit").on("click", function(event) {
     writeToStorage();
     createButton();
     // Wait for the ajax query to finish.
-    $.when(currentWeather(), forecastWeather()).done(function(response1, response2) {
+    $.when(currentWeather(), forecastWeather()).done(function(weatherData, forecastData) {
 
         // Display current weather from query 1.
-        console.log(response1);
-        $("#city").text(response1[0].name);
-        $("#date").text(dayjs.unix(response1[0].dt).format("M/D/YYYY"));
-        $("#temp").text("Temperature: " + Math.floor(response1[0].main.temp) + "\u00B0");
-        $("#humid").text("Humidity: " + response1[0].main.humidity + "%");
-        $("#wind").text("Wind speed: " + Math.floor(response1[0].wind.speed) + " mph");
+        console.log(weatherData);
+        $("#city").text(weatherData[0].name);
+        $("#date").text(dayjs.unix(weatherData[0].dt).format("M/D/YYYY"));
+        $("#temp").text("Temperature: " + Math.floor(weatherData[0].main.temp) + "\u00B0");
+        $("#humid").text("Humidity: " + weatherData[0].main.humidity + "%");
+        $("#wind").text("Wind speed: " + Math.floor(weatherData[0].wind.speed) + " mph");
+        // Save longitude and latitude data for uvIndex query.
+        cityLon = weatherData[0].coord.lon;
+        cityLat = weatherData[0].coord.lat;
+        console.log("Longitude: " + cityLon + " | " + "Latitude: " + cityLat);
 
         // Display 5 day forecast from query 2.
-        console.log(response2);
+        console.log(forecastData);
         for (var i = 0; i < 5; i++) {
-            var date = dayjs.unix(response2[0].list[i].dt).format("M/D/YYYY");
+            var date = dayjs.unix(forecastData[0].list[i].dt).format("M/D/YYYY");
             $("#day" + (i+1) + "-title").text(date);
-            $("#day" + (i+1) + "-temp").text("Temperature: " + response2[0].list[i].main.temp  + "\u00B0");
-            $("#day" + (i+1) + "-humid").text("Humidity: " + response2[0].list[i].main.humidity + "%");
+            $("#day" + (i+1) + "-temp").text("Temperature: " + forecastData[0].list[i].main.temp  + "\u00B0");
+            $("#day" + (i+1) + "-humid").text("Humidity: " + forecastData[0].list[i].main.humidity + "%");
         }
+
+        // Display UV Index from query 3.
+        uvIndex(function(uvData) {
+            console.log(uvData);
+        })
     })
 })
 
@@ -86,7 +95,18 @@ function currentWeather() {
     return $.ajax({
         url: "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + APIKey,
         method: "GET",
-        cors: true
+        cors: true,
+        async: false
+    });
+};
+
+// 5 Day Forecast API
+function forecastWeather() {
+    return $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&cnt=5&appid=" + APIKey,
+        method: "GET",
+        cors: true,
+        async: false
     });
 };
 
@@ -95,15 +115,7 @@ function uvIndex() {
     return $.ajax({
         url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude={part}&appid=" + APIKey,
         method: "GET",
-        cors: true
-    });
-};
-
-// 5 Day / 3 Hour Forecast API
-function forecastWeather() {
-    return $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&cnt=5&appid=" + APIKey,
-        method: "GET",
-        cors: true
+        cors: true,
+        async: false
     });
 };

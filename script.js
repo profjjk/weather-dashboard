b577f0a72d16e8a9db1a5c590e95010cec76539e
Jordan Kelly <jordan.j.kelly101@gmail.com>
2021-01-05 16:49:51 -0800

    save search list to local storage

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 ///// GLOBAL VARIABLES /////
 var cityName = "";
+var testCity = "London";
 var oldSearches = [];
 var APIKey = "a479558350b81143a0cd27e30716cf87";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + APIKey;
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + testCity + "&units=imperial&appid=" + APIKey;
 
+///// EVENT LISTENERS /////
+// Get the city name and add button
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+    cityName = $("#city-name").val();
+    console.log(cityName);
+    saveSearch();
+    writeToStorage();  
+})
 
 ///// FUNCTIONS /////
-$(document).ready(function() {
-    // Get the API info.
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        cors: true
-    }).then(function(response) {
-        
-    });
-
+// Get the API info.
+$.ajax({
+    url: queryURL,
+    method: "GET",
+    cors: true
+}).then(function(response) {
+    console.log(response);
 });
 
-// Create a button from the search.
-function createButton() {
-    prevSearch = $("<button>");
-    prevSearch.addClass("btn btn-light w-75");
-    prevSearch.text(cityName);
-    $("#previous-search").prepend(prevSearch);
+
+// Save user search to list.
+function saveSearch() {
+    var newCity = {city: cityName};
+    oldSearches.push(newCity);
+    console.log(oldSearches);
+}
+
+// Store search list.
+function writeToStorage() {
+    localStorage.setItem("weatherSearch", JSON.stringify(oldSearches));
 }
 
 
@@ -31,15 +43,7 @@ function createButton() {
 
 
 
-///// EVENT LISTENERS /////
-    // Get the city name and add button
-    $("#submit").on("click", function(event) {
-        event.preventDefault();
-        cityName = $("#city-name").val();
-        createButton();
 
-        
-})
 
 
 
